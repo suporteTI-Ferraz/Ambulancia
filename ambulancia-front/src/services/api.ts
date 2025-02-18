21abaedd656ab9ee68c8c41ada7bd38a31c29b76
vagnero <vagnerimperador16@gmail.com>
2025-02-18 16:28:17 -0300

    url do proton para ser utilizada

--- ambulancia-front/src/services/api.ts
+++ ambulancia-front/src/services/api.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const API_URL = 'http://localhost:8080/api';
+const token = ''; //O TOKEN PRECISA SER RECUPERADO DE ALGUMA FORMA, PODE SER PELO react-native-async-storage
 const ViaCepAPI = "https://viacep.com.br/ws/";
 
 export const API = axios.create({
     baseURL: API_URL,
     withCredentials: true,
     headers: {
+      'Authorization': `Bearer ${token}`, // Passando o token no cabeçalho
       'Content-Type': 'application/json',
     },
   });
